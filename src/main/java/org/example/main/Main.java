6334a9a4d2a633fcd9197e5249883c7e385c1a93
VusalGuliyevv <vsalquliyev@gmail.com>
2024-08-22 01:46:11 +0400

    created intro menu

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,7 +1,16 @@
 package org.example.main;
 
+import org.example.util.InputUtil;
+
 public class Main {
     public static void main(String[] args) {
 
+        while (true) {
+            int menu = InputUtil.requireNumber("What do you want to do"
+                    + "\n1. Register student"
+                    + "\n2. Show all student"
+                    + "\n3. Find student"
+                    + "\n4. Update student");
+        }
     }
 }
--- src/main/java/org/example/util/InputUtil.java
+++ src/main/java/org/example/util/InputUtil.java
@@ -1,4 +1,19 @@
 package org.example.util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+    public static String requireText(String title) {
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        return text;
+    }
+
+    public static int requireNumber(String title) {
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        return number;
+    }
 }
